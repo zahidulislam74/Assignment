dfc86f4525f325c4080cefba8547c9da63f5e49a
Zahidul Islam <zahidulislamrupom651@gmail.com>
2023-03-22 20:49:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function multiplicationTable(num){
+    var i;
+    for(i=1;i<=10;i++){
+        var row = num + " * " + i + " = " + num * i;
+        console.log(row);
+    }
+
+}
+multiplicationTable(4);
+
+
+
+
+
+
